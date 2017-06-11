9c2630e4fecd245d2cd5110a29229a99eb7a51d5
Milos Vasic <milos85vasic@gmail.com>
2017-06-11 12:17:51 +0200

    Kotlin Serbia - Dealing with Databases: Db basics.

--- KotlinSerbia-DealingwithDatabases/app/src/main/java/net/milosvasic/conferences/kotlin_serbia/database/Crud.kt
+++ KotlinSerbia-DealingwithDatabases/app/src/main/java/net/milosvasic/conferences/kotlin_serbia/database/Crud.kt
@@ -5,10 +5,18 @@ interface Crud<T> {
 
     fun insert(vararg what: T): Boolean
 
+    fun insert(what: Collection<T>): Boolean
+
     fun select(vararg what: String): List<T>
+
+    fun select(what: Collection<String>): List<T>
 
     fun update(vararg what: T): Boolean
 
+    fun update(what: Collection<T>): Boolean
+
     fun delete(vararg what: T)
+
+    fun delete(what: Collection<T>)
 
 }
