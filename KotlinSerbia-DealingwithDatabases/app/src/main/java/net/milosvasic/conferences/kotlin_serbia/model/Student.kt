e65ef3d0dfdd33abec289ec2f73ce8427117f8e9
Milos Vasic <milos85vasic@gmail.com>
2017-06-11 14:30:47 +0200

    Kotlin Serbia - Dealing with Databases: Working on database stuff.

--- KotlinSerbia-DealingwithDatabases/app/src/main/java/net/milosvasic/conferences/kotlin_serbia/model/Student.kt
+++ KotlinSerbia-DealingwithDatabases/app/src/main/java/net/milosvasic/conferences/kotlin_serbia/model/Student.kt
@@ -2,6 +2,11 @@ package net.milosvasic.conferences.kotlin_serbia.model
 
 
 // Pay attention: Data class extends abstract class.
-data class Student(val firstName: String, val lastName: String, val yearOfBirth: Int) : DbModel() {
+data class Student(
+        val firstName: String,
+        val lastName: String,
+        val yearOfBirth: Int
+
+) : DbModel() {
     override var id = 0L
 }
