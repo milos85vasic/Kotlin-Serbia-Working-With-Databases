94e2e4b9b5e87ee187e6ab36e6d127ca0f074513
Milos Vasic <milos85vasic@gmail.com>
2017-06-11 11:46:46 +0200

    Kotlin Serbia - Dealing with Databases: Basic UI.

---
+++ KotlinSerbia-DealingwithDatabases/app/src/main/java/net/milosvasic/conferences/kotlin_serbia/MainActivity.kt
@@ -0,0 +1,14 @@
+package net.milosvasic.conferences.kotlin_serbia
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import net.milosvasic.conferences.kotlin_serbia.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
